71453093166d68e488a6036445ec76b6796f0e40
rnkepler <rnkepler@gmail.com>
2017-04-27 16:36:07 -0400

    Load map from json file

---
+++ TDGame/Assets/Scripts/GameMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameMap {
+    public enum TileType { grass, road };
+
+    public string mapName = "";
+
+    public int mapWidth = 0;
+    public int mapHeight = 0;
+
+    public TileType[] tiles = new TileType[] {};
+
+    public TileType getTileType(int x, int y)
+    {
+        //Debug.Log("Getting tile at " + x + ", " + y);
+        TileType value = TileType.grass;
+
+        int index = y * mapHeight + x;
+
+        value = tiles[index];
+
+        return value;
+    }
+}
--- TDGame/Assets/Scripts/MapGeneration.cs
+++ TDGame/Assets/Scripts/MapGeneration.cs
@@ -2,35 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MapGeneration : MonoBehaviour {
 
     public GameObject tile;
 
     void Start () {
+        //
+        // Load map data from JSON file
+        TextAsset newMapData = Resources.Load("Maps/Map01") as TextAsset;
+        string mapJson = newMapData.text;
+        Debug.Log("Map is = " + mapJson, newMapData);
+        GameMap newMap = JsonUtility.FromJson<GameMap>(newMapData.text);
 
-        
-        GenerateGrid();
+        GenerateGrid(newMap);
+    }
 
-	}
-	
-	void Update () {
+    void Update () {
 		
 	}
 
-    public void GenerateGrid()
+    public void GenerateGrid(GameMap map)
     {
-         float cubeSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        float cubeSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
 
-        for (int y = 0; y < 10; y++)
+        int width = map.mapWidth;
+        int height = map.mapHeight;
+
+        for (int y = 0; y < height; y++)
         {
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < width; x++)
             {
                 GameObject cubeTile = (GameObject)Instantiate(tile, new Vector3(x * cubeSize -4.5f, y * cubeSize -4.5f, 0), Quaternion.identity);
                 cubeTile.transform.SetParent(this.transform);
-                cubeTile.name = ("Tile (" + x + ", " + y + ")");
+
+                //
+                // Get the tile type.  grass, road, ect
+                GameMap.TileType tileType = map.getTileType(x, y);
+
+                cubeTile.name = ("Tile (" + x + ", " + y + ") type: " + tileType);
             }
         }
-
-
     }
 }
